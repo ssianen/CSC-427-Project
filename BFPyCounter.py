5115473b3a9abb9ecba62b5880b2356e634dd1f9
nyanzahroch <nyanzahroch@uvic.ca>
2024-12-09 15:50:11 -0800

    feat: changed program so it does not save the kmers, and makes its own fasta file

--- BFPyCounter.py
+++ BFPyCounter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6dae47b29e42b7a50efc77c846f58ebee0bb308f3207fb7447e2bd663607297
-size 4670
+oid sha256:f43bb004104e6ec26253038256bf0fe3198a92a4d9426cec4a58fdfaf03264ec
+size 5695
---
+++ BFPyCounter_v8.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f6dae47b29e42b7a50efc77c846f58ebee0bb308f3207fb7447e2bd663607297
+size 4670
